906f5948c3cc74919e3d6e328958f40b32601f02
Sebastian Waschik <sebastian.waschik@gmx.de>
2023-10-03 17:30:10 +0200

    Remove globals from encoder/decoder module

--- json/loader.go
+++ json/loader.go
@@ -7,18 +7,18 @@ import (
 // Preload adds json to the given Lua state's package.preload table. After it
 // has been preloaded, it can be loaded using require:
 //
-//  local json = require("json")
+//	local json = require("json")
 func Preload(L *lua.LState) {
 	L.PreloadModule("json", Loader)
 }
 
 // Loader is the module loader function.
 func Loader(L *lua.LState) int {
-	registerJSONEncoder(L)
-	registerDecoder(L)
+	t := L.NewTable()
+	registerJSONEncoder(L, t)
+	registerDecoder(L, t)
 	registerJsonDecodedObject(L)
 
-	t := L.NewTable()
 	L.SetFuncs(t, api)
 	L.Push(t)
 	return 1
